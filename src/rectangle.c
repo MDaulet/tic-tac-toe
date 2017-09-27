eeb1ae3b1897a758e289f749b01fda1b0783b6ee
Mdaulet <77003820792d@gmail.com>
2017-09-27 09:16:36 +0300

    Добавление функции для проверки принадлежности точки прямоугольнику.

--- src/rectangle.c
+++ src/rectangle.c
@@ -9,12 +9,14 @@ rectangle* rectangle_create(float x, float y, float width, float height, ALLEGRO
 	rect->x = x; 
 	rect->y = y;
 
-	rect->background = al_create_bitmap(width, height);
+	float w = width - 2 * border_size;
+	float h = height - 2 * border_size;
+	rect->background = al_create_bitmap(w, h);
 	al_set_target_bitmap(rect->background);
 	al_clear_to_color(background_color);
 	//if (rect->bitmap_background == NULL) //ERROR;
 
-	rect->border = al_create_bitmap(width - 2 * border_size, height - 2 * border_size);
+	rect->border = al_create_bitmap(width, height);
 	al_set_target_bitmap(rect->border);
 	al_clear_to_color(border_color);
 	//if (rect->bitmap_border == NULL) //ERROR;
@@ -25,11 +27,11 @@ rectangle* rectangle_create(float x, float y, float width, float height, ALLEGRO
 
 void rectangle_draw(rectangle* rect)
 {
-	al_draw_bitmap(rect->background, rect->x, rect->y, 0);
+	al_draw_bitmap(rect->border, rect->x, rect->y, 0);
 
 	float x = rect->x + rect->border_size;
 	float y = rect->y + rect->border_size;
-	al_draw_bitmap(rect->border, x, y, 0);
+	al_draw_bitmap(rect->background, x, y, 0);
 }
 
 void rectangle_destroy(rectangle* rect)
@@ -37,4 +39,15 @@ void rectangle_destroy(rectangle* rect)
 	al_destroy_bitmap(rect->background);
 	al_destroy_bitmap(rect->border);
 	free(rect);
+}
+
+bool rectangle_inside(rectangle* rect, float x, float y)
+{
+	float xl = rect->x + rect->border_size;
+	float yl = rect->y + rect->border_size;
+
+	float xr = xl + al_get_bitmap_width(rect->background);
+	float yr = yl + al_get_bitmap_height(rect->background);
+
+	return (xl <= x && x <= xr && yl <= y && y <= yr);
 }
--- src/rectangle.h
+++ src/rectangle.h
@@ -1,6 +1,7 @@
 #ifndef RECT_H
 #define RECT_H
 
+#include <stdbool.h>
 #include <allegro5/allegro.h>
 
 struct rectangle
@@ -20,4 +21,5 @@ void rectangle_draw(rectangle* rect);
 
 void rectangle_destroy(rectangle* rect);
 
+bool rectangle_inside(rectangle* rect, float x, float y);
 #endif
