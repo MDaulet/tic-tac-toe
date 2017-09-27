9f01215dec1a40847e8d0fd7542cd1d92e287b44
Mdaulet <77003820792d@gmail.com>
2017-09-27 07:46:59 +0300

    Введения констант вместо численных литералов в файле title.c

--- src/title.c
+++ src/title.c
@@ -2,6 +2,9 @@
 
 #include <string.h>
 
+#define PARAM_CALC_X 0.5
+#define PARAM_CALC_Y 0.25
+
 title* title_create(rectangle* rect, const char* text, int font_size, ALLEGRO_COLOR text_color)
 {
 	title* tit = malloc(sizeof(*tit));
@@ -20,8 +23,8 @@ title* title_create(rectangle* rect, const char* text, int font_size, ALLEGRO_CO
 void title_draw(title* tit)
 {
 	rectangle_draw(tit->rect);	
-	float x = tit->rect->x + al_get_bitmap_width(tit->rect->background) / 2;
-	float y = tit->rect->y + al_get_bitmap_height(tit->rect->background) / 4;
+	float x = tit->rect->x + al_get_bitmap_width(tit->rect->background) * PARAM_CALC_X;
+	float y = tit->rect->y + al_get_bitmap_height(tit->rect->background) * PARAM_CALC_Y;
 	al_draw_text(tit->font, tit->text_color, x, y, ALLEGRO_ALIGN_CENTRE, tit->text);
 }
 
@@ -31,4 +34,15 @@ void title_destroy(title* tit)
 	al_destroy_font(tit->font);
 	free(tit->text);
 	free(tit);
+}
+
+void title_update_text(title* tit, const char* text)
+{
+	if (strcmp(tit->text, text)) {
+		if (strlen(tit->text) < strlen(text)) {
+			free(tit->text);
+			tit->text = calloc(strlen(text) + 1, sizeof(*(tit->text)));
+		}
+		strcpy(tit->text, text);
+	}
 }
