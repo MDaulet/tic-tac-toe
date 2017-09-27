978e0737626d43fa20dc874e036d5edc7e192949
Mdaulet <77003820792d@gmail.com>
2017-09-27 14:44:52 +0300

    Изменена цветовая палитра приложения.

--- src/color.h
+++ src/color.h
@@ -1,18 +1,18 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#define COLOR_WINDOW_BACKGROUND al_map_rgb(0, 0, 0)
+#define COLOR_WINDOW_BACKGROUND al_map_rgb(0xED, 0xE9, 0xE4)
 
-#define COLOR_BUTTON_BACKGROUND al_map_rgb(255, 0, 0)
-#define COLOR_BUTTON_BORDER     al_map_rgb(0, 255, 0)
-#define COLOR_BUTTON_TEXT       al_map_rgb(0, 0, 255)
+#define COLOR_BUTTON_BACKGROUND al_map_rgb(0xFF, 0x89, 0)
+#define COLOR_BUTTON_BORDER     al_map_rgb(0xA6, 0x59, 0)
+#define COLOR_BUTTON_TEXT       al_map_rgb(0xFF, 0xFF, 0xFF)
 
-#define COLOR_LABEL_BACKGROUND 	al_map_rgb(255, 0, 0)
-#define COLOR_LABEL_BORDER     	al_map_rgb(0, 255, 0)
-#define COLOR_LABEL_TEXT       	al_map_rgb(0, 0, 255)
+#define COLOR_LABEL_BACKGROUND 	al_map_rgb(0x17, 0x29, 0xB0)
+#define COLOR_LABEL_BORDER     	al_map_rgb(0x4B, 0x5C, 0xD7)
+#define COLOR_LABEL_TEXT       	al_map_rgb(0xFF, 0xFF, 0xFF)
 
-#define COLOR_CELL_BACKGROUND 	al_map_rgb(255, 0, 0)
-#define COLOR_CELL_BORDER     	al_map_rgb(0, 255, 0)
-#define COLOR_CELL_TEXT       	al_map_rgb(0, 0, 255)
+#define COLOR_CELL_BACKGROUND 	al_map_rgb(0x00, 0xB6, 0x4F)
+#define COLOR_CELL_BORDER     	al_map_rgb(0x00, 0x76, 0x33)
+#define COLOR_CELL_TEXT       	al_map_rgb(0xFF, 0xFF, 0xFF)
 
 #endif
